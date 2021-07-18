9e76e73637e34876a5cd1d8d1aec29f87e3cbefd
Mel <meljohnpualon@gmail.com>
2021-07-18 15:51:08 +0800

    added navbar toggler class

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
 'use strict'
+
+// DOM Var declarations
+const navBar = document.querySelector('.nav-top');
+
+// DOM Event Listeners
+window.addEventListener("scroll", () => {
+    let scroll = this.scrollY;
+
+    if(scroll >= 50) {
+        navBar.classList.add('toggle')
+        // console.log('toggle new class')
+    } else {
+        // console.log('remove class')
+        navBar.classList.remove('toggle')
+    }
+    
+});
