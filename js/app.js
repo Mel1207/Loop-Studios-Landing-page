67b5d731680220fd8c681c5f0a8519e14649d798
Mel <meljohnpualon@gmail.com>
2021-07-20 03:13:36 +0800

    added mobile menu and toggler

--- js/app.js
+++ js/app.js
@@ -2,8 +2,21 @@
 
 // DOM Var declarations
 const navBar = document.querySelector('.nav-top');
+const menu = document.querySelector('.hamburger-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const docuBody = document.body; 
+
+
 
 // DOM Event Listeners
+menu.addEventListener('click', () => {
+    menu.classList.toggle('menu-click')
+    mobileMenu.classList.toggle('mobile-menu-show')
+    docuBody.classList.toggle('body-hidden')
+
+    console.log(menu)
+})
+
 window.addEventListener("scroll", () => {
     let scroll = this.scrollY;
 
